0e7abfcb07e47d7091bf5a1ec7a9cb153a9ec026
leykun <leykunejigue@gmail.com>
2021-08-18 20:00:32 +0300

    added tflite package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'presentation/on_boarding_ui/onboarding_screen.dart';
 
 void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData(), home: OnBoardingScreen());
+  }
 }
--- lib/presentation/on_boarding_ui/onboarding_screen.dart
+++ lib/presentation/on_boarding_ui/onboarding_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
-    return Container();
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(title: "Welcome"),
+      ],
+    );
   }
 }
