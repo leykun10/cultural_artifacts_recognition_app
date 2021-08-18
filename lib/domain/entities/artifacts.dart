10bdb8373791d6d94c2fa576a5026f9878a923d0
leykun <leykunejigue@gmail.com>
2021-08-18 18:23:32 +0300

    added artifacts class

---
+++ lib/domain/entities/artifacts.dart
@@ -0,0 +1,11 @@
+class Artifacts {
+  String? name;
+  String? description;
+  String? imagePath;
+
+  Artifacts(String name, String description, String imagePath) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
--- lib/presentation/on_boarding_ui/onboarding_screen.dart
+++ lib/presentation/on_boarding_ui/onboarding_screen.dart
@@ -5,6 +5,7 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return Container();
   }
 }
